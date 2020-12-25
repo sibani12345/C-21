5a8b1cb22219bb378aad52510e715180dd183fbb
sibani12345 <75684228+sibani12345@users.noreply.github.com>
2020-12-25 15:57:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(100, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  fixedRect.velocityX = +5;
+  movingRect.velocityX = -5;
+
+}
+
+function draw() {
+  background(0,0,0);  
+
+  bounceoff(movingRect,fixedRect);
+
+  drawSprites();
+}
+
+ 
+
+
